235adb42365ecd1d7641e963f74f48c91b5ef7b8
Rodion Mustafaev <rodion9116@yandex.ru>
2023-10-16 22:38:54 +1000

    Add project files.

---
+++ ChatClient/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SimpleChatClient
+{
+    class Program
+    {
+        private static TcpClient? client;
+        private static NetworkStream? stream;
+        private static string? nickname;
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                client = new TcpClient("127.0.0.1", 8888); // Подключение к серверу
+                stream = client.GetStream();
+
+                Console.WriteLine("Input your Nick Name");
+                nickname = Console.ReadLine();
+                byte[] nicknameBytes = Encoding.UTF8.GetBytes(nickname);
+                stream.Write(nicknameBytes, 0, nicknameBytes.Length);
+
+                // Поток для чтения сообщений от сервера
+                Thread receiveThread = new Thread(ReceiveMessage);
+                receiveThread.Start();
+
+                // Отправка сообщений на сервер
+                while (true)
+                {
+                    string? message = Console.ReadLine();
+                    //message = nickname + ": " + message;
+                    if (!string.IsNullOrEmpty(message))
+                    {
+                        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+                        stream.Write(messageBytes, 0, messageBytes.Length);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+        }
+
+        // Функция для чтения сообщений от сервера
+        private static void ReceiveMessage()
+        {
+            while (true)
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                if (!message.StartsWith(nickname + ":"))
+                {
+                    Console.WriteLine(message);
+                }
+            }
+        }
+    }
+}
---
+++ ChatServer/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace SimpleChatServer
+{
+    class Program
+    {
+        private static readonly List<TcpClient> clients = new List<TcpClient>(); // список клиентов
+        private static readonly List<string> nicknames = new List<string>(); // список никнеймов клиентов
+
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 8888); // создание сервера
+            server.Start(); // запуск сервера
+            Console.WriteLine("Сервер запущен.");
+
+            while (true) // бесконечный цикл для прослушивания новых подключений
+            {
+                TcpClient client = server.AcceptTcpClient(); // принятие нового клиента
+                clients.Add(client); // добавление клиента в список
+
+                // запуск обработчика клиента в отдельном потоке
+                Thread clientThread = new Thread(HandleClient);
+                clientThread.Start(client);
+            }
+        }
+
+        // Обработчик клиента
+        private static void HandleClient(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+            NetworkStream stream = client.GetStream();
+            string nickname;
+
+            byte[] nicknameRequest = Encoding.UTF8.GetBytes("NICK"); // запрос на никнейм
+            stream.Write(nicknameRequest, 0, nicknameRequest.Length); // отправка запроса на никнейм
+
+            byte[] buffer = new byte[1024];
+            int bytesRead = stream.Read(buffer, 0, buffer.Length);
+            nickname = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+            nicknames.Add(nickname); // добавление никнейма в список
+
+            BroadcastMessage($"{nickname} присоединился к чату!\n"); // отправка сообщения о присоединении нового клиента
+
+            while (client.Connected)
+            {
+                try
+                {
+                    bytesRead = stream.Read(buffer, 0, buffer.Length);
+                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                    BroadcastMessage($"{nickname}: {message}");
+                }
+                catch
+                {
+                    client.Close();
+                    nicknames.Remove(nickname);
+                    clients.Remove(client);
+                    BroadcastMessage($"{nickname} покинул чат.\n");
+                    break;
+                }
+            }
+        }
+
+        // Отправка сообщения всем клиентам
+        private static void BroadcastMessage(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+            foreach (TcpClient client in clients) // отправка сообщения каждому клиенту
+            {
+                NetworkStream stream = client.GetStream();
+                stream.Write(data, 0, data.Length);
+            }
+        }
+    }
+}
